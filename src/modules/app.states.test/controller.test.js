2b158aeb3ec8e5fb673bbca2305ad8cdea4fe622
Antoine Gambro <antoine.gambro@outlook.com>
2016-11-18 16:56:12 +0100

    test

---
+++ src/modules/app.states.test/controller.test.js
@@ -0,0 +1,17 @@
+/**
+ * @memberOf app.states.test
+ */
+(function (module) {
+  'use strict';
+
+  function TestController($scope, popupService, statesData) {
+    var controller = this;
+
+    
+  }
+
+  module.controller('testController', [
+    TestController
+  ]);
+
+}(angular.module('app.states.test')));
